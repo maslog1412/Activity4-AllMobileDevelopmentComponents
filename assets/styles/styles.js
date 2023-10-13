a0a590d440cd3b2f24f57609abaa330544e07aed
maslog1412 <46992572+maslog1412@users.noreply.github.com>
2023-10-14 00:21:14 +0800

    Update missing files
    Upload assets for styles

---
+++ assets/styles/styles.js
@@ -0,0 +1,52 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    margin: 15,
+  },
+  modalContainer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    width: 300,
+  },
+  textInputContainer: {
+    marginTop: 10,
+  },
+  flatListContainer: {
+    marginTop: 10,
+    flex: 1,
+    width: '100%',
+  },
+  switchContainer: {
+    marginTop: 10,
+  },
+  mapContainer: {
+    flex: 1,
+    width: '80%',
+    maxHeight: 200,
+  },
+  checkboxContainer: {
+    marginTop: 10,
+  },
+  progressContainer: {
+    marginTop: 10,
+  },
+  toastContainer: {
+    marginTop: 10,
+  },
+  timerPickerContainer: {
+    marginTop: 10,
+  },
+});
